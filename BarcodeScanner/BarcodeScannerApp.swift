53b3ffe4984b8e8c0a520ace5a35eb20ad4161f9
Dmytro Sokotniuk <dimastasokotnyuk@gmail.com>
2025-07-08 18:30:01 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Дмитро Сокотнюк on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
